c34970a43857a59ff2a3cc2ca5551a857a93c467
Christian Rudh <christian@rudh.se>
2014-03-10 14:27:48 +0100

    Minor cleanups and comments.

--- src/LongSessionService.java
+++ src/LongSessionService.java
@@ -15,11 +15,13 @@ import java.util.Date;
 @Stateless
 @Path("/lss")
 public class LongSessionService {
+    // FIXME: Check that the HttpOnly flag is set, if older version of JAX-RS implemention it needs to be done by hand
+
     private final static int COOKIE_AGE_SECONDS = 60 * 60 * 24 * 7;
     private final static String HASH_KEY = "kallekula";
     private final static String HASH_KEY_ALGORITHM = "HmacSHA256";
     private final static String HASH_ENCODING = "UTF-8";
-    // FIXME CR: Set this to true after testing (but requires HTTPS)
+    // FIXME: Set this to true after testing (but requires HTTPS)
     private final static boolean SECURE_COOKIE = false;
 
     @GET
@@ -27,19 +29,20 @@ public class LongSessionService {
     @Produces(MediaType.TEXT_PLAIN)
     public Response doLogin(@Context HttpServletRequest req, @PathParam("userName") String userName)
             throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException {
-        Date expiryDate = new Date();
+        final Date expiryDate = new Date();
         expiryDate.setTime(expiryDate.getTime() + (COOKIE_AGE_SECONDS * 1000));
-        String expiryDateString = String.valueOf(expiryDate.getTime());
+        final String expiryDateString = String.valueOf(expiryDate.getTime());
 
-        NewCookie userCookie = createCookie("userName", userName);
-        NewCookie expiryCookie = createCookie("expiry", expiryDateString);
-        NewCookie hashCookie = createCookie("hash", hashValue(userName + expiryDateString));
+        final String contextPath = req.getContextPath();
+        final NewCookie userCookie = createCookie("userName", userName, contextPath);
+        final NewCookie expiryCookie = createCookie("expiry", expiryDateString, contextPath);
+        final NewCookie hashCookie = createCookie("hash", hashValue(userName + expiryDateString), contextPath);
 
-        HttpSession session = req.getSession(true);
+        final HttpSession session = req.getSession(true);
         session.setAttribute("userName", userName);
         session.setAttribute("loggedIn", Boolean.TRUE);
 
-        String response = "Logged in " + userName;
+        final String response = "Logged in " + userName;
 
         return Response.ok(response, MediaType.TEXT_PLAIN).cookie(userCookie, expiryCookie,
                 hashCookie).build();
@@ -49,9 +52,9 @@ public class LongSessionService {
     @Path("/status")
     @Produces(MediaType.TEXT_PLAIN)
     public String checkStatus(@Context HttpServletRequest req) {
-        HttpSession session = req.getSession(true);
-        String userName = (String)session.getAttribute("userName");
-        Boolean loggedIn = (Boolean)session.getAttribute("loggedIn");
+        final HttpSession session = req.getSession(true);
+        final String userName = (String)session.getAttribute("userName");
+        final Boolean loggedIn = (Boolean)session.getAttribute("loggedIn");
 
         return userName + " is logged in: " + loggedIn;
     }
@@ -67,30 +70,30 @@ public class LongSessionService {
         return "Validation ok: " + validateValue(userName + expiry, hash);
     }
 
-    private NewCookie createCookie(String name, String value) {
-        Date expiryDate = new Date();
+    private NewCookie createCookie(String name, String value, String path) {
+        final Date expiryDate = new Date();
         expiryDate.setTime(COOKIE_AGE_SECONDS * 1000);
 
-        Cookie cookie = new Cookie(name, value, "/", "");
+        final Cookie cookie = new Cookie(name, value, "/", path);
 
         return new NewCookie(cookie, "", COOKIE_AGE_SECONDS, expiryDate, SECURE_COOKIE, true);
     }
 
     private String hashValue(String value) throws NoSuchAlgorithmException,
             InvalidKeyException, UnsupportedEncodingException {
-        SecretKeySpec key = new SecretKeySpec(HASH_KEY.getBytes("UTF-8"), HASH_KEY_ALGORITHM);
+        final SecretKeySpec key = new SecretKeySpec(HASH_KEY.getBytes("UTF-8"), HASH_KEY_ALGORITHM);
 
-        Mac mac = Mac.getInstance(HASH_KEY_ALGORITHM);
+        final Mac mac = Mac.getInstance(HASH_KEY_ALGORITHM);
         mac.init(key);
 
-        byte[] hash = mac.doFinal(value.getBytes(HASH_ENCODING));
+        final byte[] hash = mac.doFinal(value.getBytes(HASH_ENCODING));
 
         return new String(Base64.encodeBase64(hash), HASH_ENCODING);
     }
 
     private boolean validateValue(String value, String hash) throws NoSuchAlgorithmException,
             InvalidKeyException, UnsupportedEncodingException {
-        String comparisonHash = hashValue(value);
+        final String comparisonHash = hashValue(value);
 
         return comparisonHash.equals(hash);
     }
